59c302b6080303c421c38b1a2b15fee784bd5a55
ssomeshg <121214623+ssomeshg@users.noreply.github.com>
2024-12-07 11:26:52 +0530

    loader animation added

--- js/main.js
+++ js/main.js
@@ -24,14 +24,14 @@
 	fullHeight();
 
 	// loader
-	var loader = function() {
-		setTimeout(function() { 
-			if($('#ftco-loader').length > 0) {
-				$('#ftco-loader').removeClass('show');
-			}
-		}, 1);
-	};
-	loader();
+	// var loader = function() {
+	// 	setTimeout(function() { 
+	// 		if($('#ftco-loader').length > 0) {
+	// 			$('#ftco-loader').removeClass('show');
+	// 		}
+	// 	}, 1);
+	// };
+	// loader();
 
   var carousel = function() {
   	$('.home-slider').owlCarousel({
